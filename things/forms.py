df55b5a54aff73170ed89bbd0cc566096830c92f
Maneet Sigtia <sigtiamaneet@gmail.com>
2023-12-06 16:10:14 +0000

    Add ThingForm and render it in the home view

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,15 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
 
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+    
+        widgets = {
+            'description': forms.Textarea(),   # Display description as a Textarea
+            'quantity': forms.NumberInput(),   # Display quantity as a NumberInput
+        }
